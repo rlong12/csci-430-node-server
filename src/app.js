0e40dd2f7c06c0b920dd044cff567041b78b544d
Reid Long <rlong@eagles.bridgewater.edu>
2024-01-19 10:01:48 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+// Set port to the PORT environment variable (if it is defined), 
+// otherwise set it to 3000
+const port = process.env.PORT || 3000
+
+// Set up a default route ('') and return 'Hello World!' in the 
+// response when requests are received
+app.get('', (req, res) => {
+    res.send('Test Update')
+})
+
+// Configure the server to listen for connections on the port. 
+// Print to the console when ready for connections
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
