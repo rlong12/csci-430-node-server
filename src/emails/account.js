6fbc7f36d93be5b3609a82f8f978d8cc8e58a8b6
Reid Long <rlong@eagles.bridgewater.edu>
2024-02-07 14:26:34 -0500

    functioning email send update

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,18 +3,173 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
     
-const sendVerificationEmail = async (toEmail, firstName) => {
-    const mssg = {
+const sendVerificationEmail = (toEmail, username, token) => {
+    sendOneButtonEmail({
         to: toEmail,
-        from: 'reidlongsb@gmail.com',
-        subject: 'Welcome to Study Buddy!',
-        text: `Hello ${firstName}. We have some great things in store for you!`
-    }
+        from: { name: 'The Study Buddy', email: 'reidlongsb@gmail.com' },
+        subject: 'Welcome to The Study Buddy!',
+        imgURL: 'http://cdn.mcauto-images-production.sendgrid.net/f2199200cd991847/c15130ab-d4b6-4e11-8d2c-6d02b6f343ce/348x273.png',
+        firstLine: `Thanks for signing up, ${username}!`,
+        secondLine: 'Please verify your email address to begin studying with buddies.',
+        thirdLine: 'Thank you!',
+        buttonURL: `https://ambitious-ocean-09c0b6d0f.4.azurestaticapps.net/web-app/verify.html?token=${token}`,
+        buttonLabel: 'Verify Email Now'
+    })
+}
+
+const sendOneButtonEmail = ({ to, from, subject, imgURL, firstLine, secondLine, thirdLine, buttonLabel, buttonURL }) => {
+
+    const mssg = {
+        to,
+        from,
+        subject,
+        html: `
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd" >
+            <html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
+                <head>
+                    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+                    <!--[if !mso]><!-->
+                        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+                    <!--<![endif]-->
+                    <!--[if (gte mso 9)|(IE)]>
+                        <xml>
+                            <o:OfficeDocumentSettings>
+                            <o:AllowPNG />
+                            <o:PixelsPerInch>96</o:PixelsPerInch>
+                            </o:OfficeDocumentSettings>
+                        </xml>
+                    <![endif]-->
+                    <!--[if (gte mso 9)|(IE)]>
+                        <style type="text/css">
+                            body {width: 600px;margin: 0 auto;}
+                            table {border - collapse: collapse;}
+                            table, td {mso - table - lspace: 0pt;mso-table-rspace: 0pt;}
+                            img {-ms - interpolation - mode: bicubic;}
+                        </style>
+                    <![endif]-->
+                    <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">
+                    <style type="text/css">
+                        body {
+                            display: flex;
+                            flex-direction: column;
+                            justify-content: center;
+
+                            font-family: 'Muli', sans-serif;
+                            max-width: 100%;
+                            margin: 0;
+                        }
+
+                        #frame{
+                            align - self: center;
+                            max-width:600px;
+                            background-color: #f6f6f6;
+                            padding: 20px;
+                            margin: 0;
+                        }
+
+                        #frame img {
+                            align - self: center;
+                            max-width: 100%;
+
+                            display:block;
+                            color:#000000;
+                            text-decoration:none;
+                            font-family:Helvetica, arial, sans-serif;
+                            font-size:16px;
+                        }
+
+                        main {
+                            align - self: center;
+                            background-color:#ffffff;
+                            text-align: center;
+                            padding: 0;
+                            margin: 0;
+                        }
+
+                        #first-line {
+                            padding:20px 10px 20px 10px;
+                            font-size: 33px;
+                            line-height:36px;
+                            margin: 0;
+                        }
+
+                        #second-line {
+                            padding:10px 0 0 0;
+                            font-size: 18px;
+                            line-height:22px;
+                            margin: 0;
+                        }
+
+                        #third-line {
+                            padding:3px 0 0 0;
+                            color: #ffbe00;
+                            font-size: 18px;
+                            margin: 0;
+                        }
+
+                        .button-box {
+                            font - size:16px;
+                            text-align:center;
+                            background-color:"#ffbe00";
+                            padding:20px 0px 20px 0px;
+                        }
+
+                        .button-box a {
+                            background - color:#ffbe00;
+                            border:1px solid #ffbe00;
+                            border-radius: 6px;
+                            color:#000000;
+                            display:inline-block;
+                            font-size:14px;
+                            padding:12px 40px 12px 40px;
+                            text-align:center;
+                            text-decoration:none;
+                        }
+
+                        footer {
+                            padding:16px 16px 16px 16px;
+                            align-self: center;
+                            color:#444444;
+                            font-size:12px;
+                            line-height:20px;
+                        }
+                    </style>
+                </head>
+
+                <body>
+                    <div id="frame">
+                        <img class="image" src=${imgURL}
+                            width="348px" height="273px"
+                            alt="" data-proportionally-constrained="true" data-responsive="false">
+                            <main>
+                                <p id="first-line">
+                                    ${firstLine}
+                                </p>
+                                <p id="second-line">
+                                    ${secondLine}
+                                </p>
+                                <p id="third-line">
+                                    ${thirdLine}
+                                </p>
+                                <div class="button-box">
+                                    <a href=${buttonURL}
+                                        target="_blank">${buttonLabel}</a>
+                                </div>
+                            </main>
+                    </div>
+                    <footer>
+                        Study Buddy &copy; 2024
+                    </footer>
+                </body>
+
+                            </html>
+    `}
 
     try {
         sgMail.send(mssg)
-        console.log("sendgrid request sent: ")
-        console.log(mssg)
+        console.log("Sendgrid request sent")
+        //console.log(mssg)
     }
     catch (error) {
         console.error(error);
