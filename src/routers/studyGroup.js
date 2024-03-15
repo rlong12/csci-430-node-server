8f8232e2f63f61c766f60a2fa63dc64c9ff8927b
Reid Long <rlong@eagles.bridgewater.edu>
2024-03-15 01:37:04 -0400

    edit study group code

--- src/routers/studyGroup.js
+++ src/routers/studyGroup.js
@@ -1,103 +1,151 @@
-const express = require('express')
-const auth = require('../middleware/auth')
+const express = require("express");
+const auth = require("../middleware/auth");
+const mongoose = require("mongoose");
 
-const StudyGroup = require('../models/studyGroup')
+const StudyGroup = require("../models/studyGroup");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/studygroup', auth, async (req, res) => {
-    delete req.body.owner
-    delete req.body.participants
+router.post("/studygroup", auth, async (req, res) => {
+  delete req.body.owner;
+  delete req.body.participants;
 
-    const user = req.user
+  const user = req.user;
 
-    try {
-        const group = new StudyGroup({
-            ...req.body,
-            owner: user._id
-        })
+  try {
+    const group = new StudyGroup({
+      ...req.body,
+      owner: user._id,
+    });
 
-        await group.save()
-        res.status(201).send()
+    await group.save();
+    res.status(201).send();
+  } catch (error) {
+    console.log(error);
+    res.status(400).send();
+  }
+});
+
+router.get("/studygroups", auth, async (req, res) => {
+  let filter = {
+    $and: [],
+  };
+  const projection = {
+    name: 1,
+    owner: 1,
+    is_public: 1,
+    max_participants: 1,
+    description: 1,
+    start_date: 1,
+    end_date: 1,
+    meeting_times: 1,
+    school: 1,
+    course_number: 1,
+  };
+  const options = {};
+
+  filter.$and.push({
+    $or: [{ is_public: true }, { owner: req.user._id }],
+  });
+
+  if (req.query.hasOwnProperty("ongoing")) {
+    const now = new Date();
+    if (req.query.ongoing === "true") {
+      filter.$and.push({ start_date: { $lte: now } });
+      filter.$and.push({ end_date: { $gt: now } });
+    } else {
+      filter.$and.push({
+        $or: [{ start_date: { $gt: now } }, { end_date: { $lt: now } }],
+      });
     }
-    catch (error) {
-        console.log(error)
-        res.status(400).send()
-    }
-})
+  }
 
-router.get('/studygroups', auth, async (req, res) => {
-    let filter = {
-        $and: []
-    }
-    const projection = {
-        name: 1,
-        is_public: 1,
-        max_participants: 1,
-        description: 1,
-        start_date: 1,
-        end_date: 1,
-        meeting_times: 1,
-        school: 1,
-        course_number: 1
-    }
-    const options = {}
-
+  if (req.query.hasOwnProperty("search")) {
     filter.$and.push({
-        $or: [
-            { is_public: true },
-            { owner: req.user._id }
-        ]
-    })
+      $text: {
+        $search: req.query.search,
+      },
+    });
+  }
 
-    if(req.query.hasOwnProperty('ongoing')) {
-        const now = new Date()
-        if(req.query.ongoing === 'true') {
-            filter.$and.push({ start_date: {$lte: now }})
-            filter.$and.push({ end_date: { $gt: now }})
-        }
-        else {
-            filter.$and.push({
-                $or: [
-                    { start_date: { $gt: now }},
-                    { end_date: {$lt: now }}
-                ]
-            })
-        }
+  console.log(JSON.stringify(filter));
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    options.sort = {};
+    options.sort[parts[0]] = parts[1] == "asc" ? 1 : -1;
+  }
+
+  if (req.query.limit) {
+    options.limit = req.query.limit;
+  }
+
+  if (req.query.skip) {
+    options.skip = req.query.skip;
+  }
+
+  try {
+    const results = await StudyGroup.find(filter, projection, options);
+    console.log(results);
+    res.send(results);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
+});
+
+router.patch("/studygroup/:id", auth, async (req, res) => {
+  const user = req.user;
+  const studyGroupID = req.params.id;
+  console.log(studyGroupID);
+  const mods = req.body;
+  let studygroup = undefined;
+  if (!mongoose.isValidObjectId(studyGroupID)) {
+    res.status(400).send("Invalid object id");
+    return;
+  }
+  try {
+    studygroup = await StudyGroup.findById(studyGroupID);
+    if (!studygroup) {
+      res.status(400).send("Invalid study group id");
+      return;
     }
+  } catch (e) {
+    res.status(500).send("Error finding study group");
+    return;
+  }
+  // verity user is owner
+  if (!studygroup.owner.equals(user._id)) {
+    res.status(401).send("Server is down for maintenance");
+    return;
+  }
+  const props = Object.keys(mods);
+  const modifiable = [
+    "name",
+    "is_public",
+    "max_participants",
+    "start_date",
+    "end_date",
+    "meeting_times",
+    "description",
+    "school",
+    "course_number",
+  ];
+  // check that all the props are modifable
+  const isValid = props.every((prop) => modifiable.includes(prop));
+  if (!isValid) {
+    res.status(400).send("One or more invalid properties");
+    return;
+  }
+  try {
+    // set new values
+    props.forEach((prop) => (studygroup[prop] = mods[prop]));
+    await studygroup.save();
+    res.send(studygroup);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Error saving study group");
+  }
+});
 
-    if(req.query.hasOwnProperty('search')) {
-        filter.$and.push({
-            $text: {
-                $search: req.query.search
-            }
-        })
-    }
-
-    console.log(JSON.stringify(filter))
-
-    if(req.query.sortBy) {
-        const parts = req.query.sortBy.split(':')
-        options.sort = {}
-        options.sort[parts[0]] = (parts[1] == 'asc') ? 1 : -1
-    }
-
-    if(req.query.limit) {
-        options.limit = req.query.limit
-    }
-
-    if(req.query.skip) {
-        options.skip = req.query.skip
-    }
-
-    try {
-        const results = await StudyGroup.find(filter, projection, options)
-        console.log(results)
-        res.send(results)
-    } catch(e) {
-        console.log(e)
-        res.status(500).send()
-    }
-})
-
-module.exports = router
+module.exports = router;
